d204c4c644249ae1dba9c27a31068ad245073994
Ogurana17 <ogurana17@gmail.com>
2023-01-12 15:03:21 +0900

    基礎的な動作を実装

--- js/shinyProbability.js
+++ js/shinyProbability.js
@@ -1,1 +1,24 @@
 // ShinyProbability
+document.getElementById('traialsInputButton').onclick = function addCnt () {
+    //初期設定
+    const traialsInput = document.getElementById('traialsInput');
+    const nextShinyProbabilityTitle = document.getElementById('nextShinyProbabilityTitle');
+    const shingleShinyProbabilityInput = document.getElementById('shingleShinyProbabilityInput');
+
+    //演算
+    traialsInput.value++;
+    nextShinyProbabilityTitle.innerHTML = '✨' + ((1 - (Math.pow(1 - (1 / shingleShinyProbabilityInput.value), traialsInput.value)))*100).toFixed(3) + '%✨';
+
+    //log書き込み
+    console.log(traialsInput.value);
+    console.log(nextShinyProbabilityTitle.innerHTML);
+}
+
+//inputnumberの有効桁数制限
+document.getElementById('traialsInput').oninput = function sliceMaxLength() {
+    traialsInput.value = traialsInput.value.slice(0, 6);
+}
+
+document.getElementById('shingleShinyProbabilityInput').oninput = function sliceMaxLength() {
+    shingleShinyProbabilityInput.value = shingleShinyProbabilityInput.value.slice(0, 6);
+}
